d801d428afa46dac41c8beb7eaf78b9f202803b1
tunnelrat16 <tjandmatt@hotmail.com>
2022-05-27 16:35:46 -0700

    initial commit

--- App.java
+++ App.java
@@ -1,6 +1,22 @@
 class App {
   public static void main(String args[]){
-    String message = "Hello, world!";
-    System.out.println(message);
+    int vehicleIdentificationNumber = 1234567;
+    String vehicleColor = "Pink";
+    String vehicleMake = "Cadillac";
+    String vehicleModel = "Escalade";
+    boolean towingPackage = true;
+    int odometerReading = 10_124;
+    double price = 45_009.99;
+    String qualityRating  = "C";
+    String newLine = System.getProperty("line.separator");
+
+    System.out.println(" VIN: " + vehicleIdentificationNumber + newLine +
+    " Color: " + vehicleColor + newLine +
+    " Make: " + vehicleMake + newLine +
+    " Model: " + vehicleModel + newLine +
+    " Towing Package Included? " + towingPackage + newLine +
+    " Current Odometer Reading: " + odometerReading + newLine +
+    " Unit Price: " + price + newLine +
+    " Unit Quality Rating: " + qualityRating);
   }
 }
